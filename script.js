8c5d08b3a7f22c28c7460ed16213300d4dc6975d
BonesTheGhost <bonestheghost@gmail.com>
2020-07-14 10:49:06 -0500

    [003-(0.0.3)]:: changing attributes >> Have now added the ability to update attributes based on other inputs on the page. WIll expand this to take into account user input data. This data will be key to the logic algorithms.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log("[script.js]:: Attached and working properly!");
 
+document.getElementById("createInputButton").onclick = function() {
+  let mapHeight = document.getElementById("mapHeight").value;
+  let mapWidth = document.getElementById("mapWidth").value;
+  console.log("[Map Dimensions (H x W)]:: ", mapHeight, " x ", mapWidth);
+
+  //update the textarea size for easier input.
+  document.getElementById("mapInput").setAttribute("class", "show");
+}
+
+
+
+
+//This reads the stuff in the input area, but cannot differentiate between lines...
 document.getElementById("generateButton").onclick = function() {
 
   let lines = document.getElementById("mapInput").value.split('\n');
