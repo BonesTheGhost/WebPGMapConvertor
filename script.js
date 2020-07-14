36b7d63c62182327e2f905725bc9d91024ec4322
BonesTheGhost <bonestheghost@gmail.com>
2020-07-14 11:26:20 -0500

    [004-(0.0.5)]:: sorting input >> have successfully created a series of loops that run in their entirety based on map dimensions to determine where the characters should be stored. Next is to check that the inputs are being stored in the correct place, and can be read out in the correct order.

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 console.log("[script.js]:: Attached and working properly!");
 
+//global scope so they can be referenced in later logic and update from anywhere.
+let mapHeight = 20;
+let mapWidth = 20;
+
+//an array to hold the chars of textarea to increment through later.
+let textAreaContent = [];
+
+let column = [];
+
+//global map container
+let mapArray = [];
+
 document.getElementById("createInputButton").onclick = function() {
-  let mapHeight = document.getElementById("mapHeight").value;
-  let mapWidth = document.getElementById("mapWidth").value;
+  mapHeight = document.getElementById("mapHeight").value;
+  mapWidth = document.getElementById("mapWidth").value;
   console.log("[Map Dimensions (H x W)]:: ", mapHeight, " x ", mapWidth);
 
   //update the textarea size for easier input.
+  document.getElementById("mapInput").setAttribute("row", mapHeight);
+  document.getElementById("mapInput").setAttribute("cols", mapWidth);
   document.getElementById("mapInput").setAttribute("class", "show");
 }
-
-
-
 
 //This reads the stuff in the input area, but cannot differentiate between lines...
 document.getElementById("generateButton").onclick = function() {
 
-  let lines = document.getElementById("mapInput").value.split('\n');
+  textareaContent = document.getElementById("mapInput").value.split("");
+  console.log("textareaContent: ", textareaContent);
 
-  console.log("lines: ", lines);
+  createMapData();
+}
+
+createMapData = () => {
+  numberOfColumns = mapWidth;
+
+  for(i = 0; i <= (numberOfColumns - 1); i++){
+    //empty the column
+    column = [];
+
+    for(rowControl = 0; rowControl <= (mapHeight - 1); rowControl++){
+      column.push(textAreaContent[rowControl]);
+    }
+
+    mapArray.push(column);
+  }
+
+  console.log("[mapArray]:: ", mapArray);
 }
