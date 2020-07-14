7e9ca65726cd7ee9b59dc9d342ea18f60a708ae7
BonesTheGhost <bonestheghost@gmail.com>
2020-07-14 14:57:46 -0500

    [008-(0.0.9)]:: character counter >> Attempted to automitically size the textarea more accurately. Refined logic such that it works on 20 x 20, but ran into problems with other sizes. Added a character counter based on dimensions so that independent of textarea size the number of characters is at least correct. Made the generate button disable if too many characters.

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ console.log("[script.js]:: Attached and working properly!");
 //Test string:: ####################$$$$$$$$$$$$$$$$$$$$%%%%%%%%%%%%%%%%%%%%aaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbccccccccccccccccccccddddddddddddddddddddeeeeeeeeeeeeeeeeeeeeffffffffffffffffffffgggggggggggggggggggghhhhhhhhhhhhhhhhhhhhiiiiiiiiiiiiiiiiiiiijjjjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkkkkkllllllllllllllllllllmmmmmmmmmmmmmmmmmmmmnnnnnnnnnnnnnnnnnnnnooooooooooooooooooooppppppppppppppppppppqqqqqqqqqqqqqqqqqqqq
 
 //global scope so they can be referenced in later logic and update from anywhere.
-let mapHeight = 20;
-let mapWidth = 20;
+let mapHeight = 1;
+let mapWidth = 1;
+let tooManyCharacters = false;
 
 let totalChars = mapHeight * mapWidth;
 let charCursor = 0;
@@ -17,6 +18,7 @@ let column = [];
 //global map container
 let mapArray = [];
 
+//get Parameters and display map input.
 document.getElementById("createInputButton").onclick = function() {
   mapHeight = document.getElementById("mapHeight").value;
   mapWidth = document.getElementById("mapWidth").value;
@@ -25,33 +27,56 @@ document.getElementById("createInputButton").onclick = function() {
   console.log("[Map Dimensions (H x W) : total chars]:: ", mapHeight, " x ", mapWidth, " : ", totalChars);
 
   //update the textarea size for easier input.
-  //WIDTH OF TEXTAREA IS 0-BASED, HEIGHT IS NOT!!!
-  document.getElementById("mapInput").setAttribute("row", mapHeight);
-  document.getElementById("mapInput").setAttribute("cols", mapWidth - 1);
-  document.getElementById("mapInput").setAttribute("class", "show");
+  //Spent a TON of time messing with this. Textarea does NOT scale consistently... period.
+  document.getElementById("mapInput").setAttribute("rows", mapHeight);
+  document.getElementById("mapInput").setAttribute("cols", mapWidth);
+  //display the text area.
+  document.getElementById("mapInput").setAttribute("class", "show textarea");
 }
 
-//This reads the stuff in the input area, but cannot differentiate between lines...
+document.getElementById("mapInput").onkeyup = function() {
+  if(charLimit(this.value)){
+    console.log("too many characters!")
+  };
+}
+
+//This reads the stuff in the input area and spreads into textAreaContent[].
 document.getElementById("generateButton").onclick = function() {
 
-  //grab the entire string with no \n or spaces
+  if(!tooManyCharacters){
+    //grab the entire string with no \n or spaces
   contentString = document.getElementById("mapInput").value.split("");
   //spread it into textAreaContent (split may be redundant).
   textAreaContent = [...contentString];
   console.log(textAreaContent);
   
   createMapData();
+  }else{
+    document.getElementById("outputSection").innerHTML = "too many characters too generate!";
+  }
+  
 }
 
+function charLimit(currentChars){
+  if(currentChars.length > totalChars){
+    tooManyCharacters = true;
+    return true;
+  } else
+  return false;
+}
+
+//Copies each character of textAreaContent[] of nth (mapWidth) index into a column array. Then pushes all columns into mapArray[].
 sortColumns = () => {
   for(i=0; i<mapWidth; i++){
     //reset the column container
     column=[];
 
-    for(a=i; a<totalChars; a+=mapHeight){
-      let char = textAreaContent[a];
+    for(charCursor; charCursor<totalChars; charCursor+=mapHeight){
+      let char = textAreaContent[charCursor];
       column.push(char);
     }
+    charCursor++;
+
     mapArray.push(column);
   }
 }
