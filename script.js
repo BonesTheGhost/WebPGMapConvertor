b9c7f1e0ccb4d0591338f2834488f69cd2e32dd4
BonesTheGhost <bonestheghost@gmail.com>
2020-07-14 11:48:31 -0500

    [006-(0.0.7)]:: lunch break, logic still broken.

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ console.log("[script.js]:: Attached and working properly!");
 //Test string:: ####################$$$$$$$$$$$$$$$$$$$$%%%%%%%%%%%%%%%%%%%%aaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbccccccccccccccccccccddddddddddddddddddddeeeeeeeeeeeeeeeeeeeeffffffffffffffffffffgggggggggggggggggggghhhhhhhhhhhhhhhhhhhhiiiiiiiiiiiiiiiiiiiijjjjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkkkkkllllllllllllllllllllmmmmmmmmmmmmmmmmmmmmnnnnnnnnnnnnnnnnnnnnooooooooooooooooooooppppppppppppppppppppqqqqqqqqqqqqqqqqqqqq
 
 //global scope so they can be referenced in later logic and update from anywhere.
-let mapHeight = 19;
-let mapWidth = 19;
+let mapHeight = 20;
+let mapWidth = 20;
+
+let totalChars = mapHeight * mapWidth;
+let charCursor = 0;
 
 //an array to hold the chars of textarea to increment through later.
 let textAreaContent = [];
@@ -17,7 +20,8 @@ let mapArray = [];
 document.getElementById("createInputButton").onclick = function() {
   mapHeight = document.getElementById("mapHeight").value;
   mapWidth = document.getElementById("mapWidth").value;
-  console.log("[Map Dimensions (H x W)]:: ", mapHeight, " x ", mapWidth);
+  totalChars = mapHeight * mapWidth;
+  console.log("[Map Dimensions (H x W) : total chars]:: ", mapHeight, " x ", mapWidth, " : ", totalChars);
 
   //update the textarea size for easier input.
   //WIDTH OF TEXTAREA IS 0-BASED, HEIGHT IS NOT!!!
@@ -36,18 +40,14 @@ document.getElementById("generateButton").onclick = function() {
 }
 
 createMapData = () => {
-  numberOfColumns = mapWidth;
+  
+  for(i=0; i < mapHeight; i++){
+    column.push(textAreaContent[charCursor]);
+    charCursor++;
 
-  for(i = 0; i <= (numberOfColumns - 1); i++){
-    //empty the column
-    column = [];
-
-    for(rowControl = 0; rowControl <= (mapHeight - 1); rowControl++){
-      column.push(textAreaContent[rowControl]);
-    }
-
-    mapArray.push(column);
+    console.log("[column]:: ", column);
   }
 
+  mapArray.push(column);
   console.log("[mapArray]:: ", mapArray);
 }
